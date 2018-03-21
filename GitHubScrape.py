54829c80bf5145e07ba570747edbc41f0b1913c1
Hussain Humadi <hah4sk@virginia.edu>
2018-03-21 11:42:03 -0400

    Added Recursive functionality to go through all subdirectories

--- GitHubScrape.py
+++ GitHubScrape.py
@@ -1,12 +1,12 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
-
-
+import time
 
 def getRepoDump(base_url, filename):
 
     # opening up connection, grabbing the page
     uClient = uReq(base_url)
+    time.sleep(.500)
     page_html = uClient.read()
     # close the client
     uClient.close()
@@ -24,6 +24,8 @@ def getRepoDump(base_url, filename):
     name = name[0:index - 1]
     print(name)
     f.write(name + '\'s Repos\n')
+    username = name[0:name.find(' ')]
+    print(username)
 
     #for each repo
     for container in containers:
@@ -38,6 +40,7 @@ def getRepoDump(base_url, filename):
         repo_url = base_url + '/' + repoTitle
         # opening up connection, grabbing the repo page
         uClient = uReq(repo_url)
+        time.sleep(.500)
         page_html = uClient.read()
         # close the Client
         uClient.close()
@@ -46,13 +49,34 @@ def getRepoDump(base_url, filename):
         page_soup = soup(page_html, "html.parser")
         subContainers = page_soup.findAll("td", {"class": "content"})
         for subContainer in subContainers:
-            itemTitle = subContainer.text
+            itemTitle = subContainer.text[1:]
             if (subContainer != subContainers[0]):
                 f.write(itemTitle)
-        f.write("#########################\n")
+                href = subContainer.span.a["href"]
+                print(href)
+                subDir_url = "https://github.com" + href
+                scrapeSubDir(subDir_url, f, "-----")
+        f.write("##########################################\n")
 
 
+def scrapeSubDir(directory_url, f, indent):
 
+    # opening up connection, grabbing the repo page
+    uClient = uReq(directory_url)
+    time.sleep(.500)
+    #uClient = uReq("https://github.com/hah4sk/DragonBall-Fighter-Game/tree/master/Backgrounds")
+    page_html = uClient.read()
+    # close the Client
+    uClient.close()
 
+    # html parsing
+    page_soup = soup(page_html, "html.parser")
+    containers = page_soup.findAll("td", {"class": "content"})
 
-
+    for container in containers:
+        itemTitle = container.text[1:]
+        if container != containers[0]:
+            f.write(indent + itemTitle)
+            href = container.span.a["href"]
+            print(href)
+            scrapeSubDir("https://github.com" + href, f, indent+"-----")
