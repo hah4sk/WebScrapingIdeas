8e297a33b156721a4fb25bf04baf57f2de49e50e
Hussain Humadi <hah4sk@virginia.edu>
2018-03-22 23:11:13 -0400

    Added GetSoup function to make establishing html connection easier

--- GitHubScrape.py
+++ GitHubScrape.py
@@ -2,16 +2,9 @@ from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 import time
 
-def getRepoDump(base_url, filename):
 
-    # opening up connection, grabbing the page
-    uClient = uReq(base_url)
-    time.sleep(.500)
-    page_html = uClient.read()
-    # close the client
-    uClient.close()
-    # html parsing
-    page_soup = soup(page_html, "html.parser")
+def getRepoDump(base_url, filename):
+    page_soup = getSoup(base_url)
     containers = page_soup.findAll("div", {"class": "pinned-repo-item-content"})
 
     #create file
@@ -23,60 +16,58 @@ def getRepoDump(base_url, filename):
     index = name.find('·')
     name = name[0:index - 1]
     print(name)
-    f.write(name + '\'s Repos\n')
+    f.write(name.upper() + '\'S REPOSITORIES\n')
     username = name[0:name.find(' ')]
-    print(username)
+    #print(username)
 
     #for each repo
     for container in containers:
         repoTitle = container.span.a.span["title"]
         repoDescription = container.p.text
-        f.write(repoTitle + '\n')
-        if (repoDescription == ""):
+        f.write("Repository Title: " + repoTitle + '\n')
+        f.write("Repository Description: ")
+        if repoDescription == "":
             f.write("No Description Available\n")
         else:
             f.write(repoDescription + '\n')
 
+        f.write("Directories/Files: \n")
         repo_url = base_url + '/' + repoTitle
         # opening up connection, grabbing the repo page
-        uClient = uReq(repo_url)
-        time.sleep(.500)
-        page_html = uClient.read()
-        # close the Client
-        uClient.close()
-
-        # html parsing
-        page_soup = soup(page_html, "html.parser")
+        page_soup = getSoup(repo_url)
         subContainers = page_soup.findAll("td", {"class": "content"})
         for subContainer in subContainers:
             itemTitle = subContainer.text[1:]
-            if (subContainer != subContainers[0]):
+            if subContainer != subContainers[0]:
                 f.write(itemTitle)
                 href = subContainer.span.a["href"]
                 print(href)
-                subDir_url = "https://github.com" + href
-                scrapeSubDir(subDir_url, f, "-----")
+                subdir_url = "https://github.com" + href
+                scrapeSubDir(subdir_url, f, "-----")
         f.write("##########################################\n")
 
 
 def scrapeSubDir(directory_url, f, indent):
 
-    # opening up connection, grabbing the repo page
-    uClient = uReq(directory_url)
-    time.sleep(.500)
-    #uClient = uReq("https://github.com/hah4sk/DragonBall-Fighter-Game/tree/master/Backgrounds")
-    page_html = uClient.read()
-    # close the Client
-    uClient.close()
-
-    # html parsing
-    page_soup = soup(page_html, "html.parser")
+    #get html parsed soup
+    page_soup = getSoup(directory_url)
     containers = page_soup.findAll("td", {"class": "content"})
 
     for container in containers:
-        itemTitle = container.text[1:]
+        itemtitle = container.text[1:]
         if container != containers[0]:
-            f.write(indent + itemTitle)
+            f.write(indent + itemtitle)
             href = container.span.a["href"]
             print(href)
             scrapeSubDir("https://github.com" + href, f, indent+"-----")
+
+def getSoup(url):
+    # opening up connection, grabbing the page
+    uClient = uReq(url)
+    time.sleep(.500)
+    page_html = uClient.read()
+    # close the client
+    uClient.close()
+    # html parsing
+    page_soup = soup(page_html, "html.parser")
+    return page_soup
